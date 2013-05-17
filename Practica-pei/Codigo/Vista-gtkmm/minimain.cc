cd0b60c16f86dd13afa5352fff0281fa6d30cdb7
Víctor Guzmán <fawques@gmail.com>
2013-05-17 12:07:07 +0200

    Algo de limpieza. minimain algo más completo. plantilla vacía (solo tiene el contenedor de pestañas)

--- Practica-pei/Codigo/Vista-gtkmm/minimain.cc
+++ Practica-pei/Codigo/Vista-gtkmm/minimain.cc
@@ -5,12 +5,29 @@ int main (int argc, char **argv){
     Glib::RefPtr<Gtk::Builder> builder_registro = Gtk::Builder::create_from_file("formulario_registro.glade");
     Gtk::Window* pWindow = 0;
   	builder->get_widget("window1", pWindow);
-  	Gtk::Alignment* pPag1 = 0;
-  	builder->get_widget("pag1", pPag1);
+  	/*Gtk::Alignment* pPag1 = 0;
+  	builder->get_widget("pag1", pPag1);*/
+  	Gtk::Notebook* pPestanas = 0;
+  	builder->get_widget("notebook1", pPestanas);
+  	
 
   	Gtk::Widget* subventana = 0;
-  	builder_registro->get_widget("box1", subventana);
-  	pPag1->add(*subventana);
+  	builder_registro->get_widget("contenedor_principal", subventana);
+
+  	// tienes que recrear el formulario si quieres que aparezca varias veces. Si no intenta asignar dos padres distintos a un mismo widget
+  	builder_registro = Gtk::Builder::create_from_file("formulario_registro.glade");
+  	Gtk::Widget* subventana2 = 0;
+  	builder_registro->get_widget("contenedor_principal", subventana2);
+  	//pPag1->add(*subventana);
+
+  	Gtk::Label* nombre_pestana = new Gtk::Label();
+  	nombre_pestana->set_text("prueba");
+  	pPestanas->append_page(*subventana, *nombre_pestana);
+  	Gtk::Label* nombre_pestana2 = new Gtk::Label();
+  	nombre_pestana2->set_text("prueba2");
+  	pPestanas->append_page(*subventana2, *nombre_pestana2);
+  	delete nombre_pestana;
+  	delete nombre_pestana2;
 
   	return app->run(*pWindow);
 }
