68030abc746f78b78fa4bce58039e87052800ba2
Víctor Guzmán <fawques@gmail.com>
2013-05-17 11:30:35 +0200

    minimain de ejemplo que muestra la plantilla y uno de los formularios dentro

--- Practica-pei/Codigo/Vista-gtkmm/minimain.cc
+++ Practica-pei/Codigo/Vista-gtkmm/minimain.cc
@@ -1,13 +1,16 @@
-#include <libglademm/xml.h>
 #include <gtkmm.h>
 int main (int argc, char **argv){
-	Gtk::Main kit(argc, argv);
-//Load the Glade file and instiate its widgets:
-	Glib::RefPtr<Gnome::Glade::Xml> refXml;
-	try
-	{
-		refXml = Gnome::Glade::Xml::create("prueba.glade");
-	}
-	catch(const Gnome::Glade::XmlError& ex)
-	{}
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv,"org.gtkmm.examples.base");
+    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("plantilla.glade");
+    Glib::RefPtr<Gtk::Builder> builder_registro = Gtk::Builder::create_from_file("formulario_registro.glade");
+    Gtk::Window* pWindow = 0;
+  	builder->get_widget("window1", pWindow);
+  	Gtk::Alignment* pPag1 = 0;
+  	builder->get_widget("pag1", pPag1);
+
+  	Gtk::Widget* subventana = 0;
+  	builder_registro->get_widget("box1", subventana);
+  	pPag1->add(*subventana);
+
+  	return app->run(*pWindow);
 }
